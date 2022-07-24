dbfdb1a424f88c43e64f5dd1768577ebd8abdbd8
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2022-07-24 16:27:05 +0800

    build: update build script

--- packages/strawberry-farm/scripts/build.mjs
+++ packages/strawberry-farm/scripts/build.mjs
@@ -8,10 +8,20 @@ import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import renameNodeModules from 'rollup-plugin-rename-node-modules';
 import { babel } from '@rollup/plugin-babel';
 
+const root = path.resolve(fileURLToPath(import.meta.url), '../..');
+
 const runBuild = async () => {
+
+  const input =
+    Object.fromEntries(
+      fs.readdirSync(path.resolve(root, 'src')).filter(n => !['theme', 'index.ts'].includes(n)).map(n => [`${n}/index`, `src/${n}/index.ts`]));
+
+  console.log(input)
   const inputOptions = {
-    input: 'src/index.ts',
-    treeshake: false,
+    input: {
+      index: "src/index.ts",
+      ...input
+    },
     external: ['async-validator', '@popperjs/core', 'react-popper', 'zustand'],
     plugins: [
       renameNodeModules(),
@@ -29,10 +39,7 @@ const runBuild = async () => {
   const outOptions = {
     dir: 'dist',
     format: 'esm',
-    chunkFileNames: '[name].js',
-    entryFileNames: '[name].js',
-    preserveModules: true,
-    preserveModulesRoot: 'src',
+    chunkFileNames: 'chunks/[name]-[hash].js',
   };
 
   const bundle = await rollup(inputOptions);
@@ -42,8 +49,6 @@ const runBuild = async () => {
 console.time();
 
 await runBuild();
-
-const root = path.resolve(fileURLToPath(import.meta.url), '../..');
 
 fs.copySync(path.resolve(root, 'src/theme'), path.resolve(root, 'dist/theme'));
 fs.copySync(path.resolve(root, 'package.json'), path.resolve(root, 'dist/package.json'));
