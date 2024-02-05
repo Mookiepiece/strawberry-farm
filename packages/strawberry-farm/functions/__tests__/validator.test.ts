c3e8a83ea7fc2bab415b74bfd1d07c3e2d5deb6a
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2024-02-05 19:02:29 +0800

    z

--- packages/strawberry-farm/functions/__tests__/validator.test.ts
+++ packages/strawberry-farm/functions/__tests__/validator.test.ts
@@ -1,173 +1,97 @@
 import { describe, it, expect } from 'vitest';
-import { Rules, validate } from '../validator';
+import { IRuleType, RuleS, validate, defineRule } from '../validator';
+
+const d = defineRule;
 
 /**
  * NN: Not Nullish
+ *
+ * C: Commented
  */
-const cc: Record<string, any> = {
-  Any1: [],
-  Any2: {},
-  Any3: { message: 'foo' },
-  Any4: { required: false, message: 'foo' },
-  Any5: 'foo',
-  Any6: ['foo', 'bar'],
-  AnyNN1: 'required',
-  AnyNN2: ['required'],
-  AnyNN3: { required: true },
-  AnyNN4: [{ required: true }, 'required', 'required'],
-  AnyNNC1: { required: 'foo' },
-  AnyNNC2: { required: true, message: 'foo' },
-  AnyNNC3: [{ required: true, message: 'foo' }, 'required', 'required'],
+const rr = {
+  Any___1: d('any'),
+  Any___2: d({}),
+  Any___3: d({ message: 'foo' }),
+  Any___4: d({ required: false, message: 'foo' }),
+  AnyNN_1: d('any!'),
+  AnyNN_2: d(['any!']),
+  AnyNN_3: d({ required: true }),
+  AnyNN_4: d({ type: 'any', required: true }),
+  AnyNNC1: d({ required: true, message: 'foo' }),
   //
-  String1: 'string',
-  String2: ['string'],
-  String3: { type: 'string' },
-  String4: [{ type: 'string' }, 'string'],
-  StringNN1: ['string', 'required'],
-  StringNN2: [{ type: 'string', required: true }],
-  StringNN3: [{ type: 'string', required: true }, 'string', 'required'],
-  StringNN4: [{ required: true }, 'string'],
-  StringNN5: ['required', { type: 'string' }],
-  StringC1: { type: 'string', message: 'foo' },
-  StringC2: [{ type: 'string' }, 'foo'],
-  StringC3: [{ type: 'string' }, { message: 'foo' }],
-  StringNNC: { type: 'string', required: 'foo' },
-  StringCNNC: { string: 'foo', required: 'bar' },
-  StringExact: { type: 'string', config: 3 },
-  StringExact2: { type: 'string', config: [3, 3] },
-  StringExactNN1: { type: 'string', config: [3, 3], required: true },
-  StringExactNN2: [{ type: 'string' }, { config: [3, 3] }, { required: true }],
-  StringExactNNC1: [
-    { type: 'string' },
-    { config: [3, 3] },
-    { required: true },
-    'foo',
-  ],
-  StringExactNNC2: [
-    { type: 'string' },
-    { config: [3, 3] },
-    { required: true },
-    { message: 'foo' },
-  ],
-  StringExactNNC3: ['string', 'required', [3, 3], 'foo'],
-  StringMin: { type: 'string', config: [3] },
-  StringMax: { type: 'string', config: [, 10] },
-  StringRange1: { type: 'string', config: [3, 10] },
-  StringRange2: { type: 'string', config: [-Infinity, Infinity] },
-  StringRangeC1: { type: 'string', config: [3, 10], message: 'foo' },
-  StringRangeC2: [
-    { type: 'string', config: true, message: 'foo' },
-    { type: 'string', config: [3, 10], message: 'bar' },
-  ],
-  StringRangeC3: [
-    { string: 'foo', required: 'duang' },
-    { string: [3, 10], message: 'bar' },
-  ],
-  //
-  'Number Nullish Literal': 'number',
-  'Number Type Required': ['number', 'required'],
-  'Number Min': { number: [3] },
-  'Number Max': { number: [, 10] },
-  'Number Range': { number: [3, 10] },
-  //
-  'Formats Email': { email: true },
-  'Formats Email Literal': 'email',
-  'Formats Email Literal Required': ['email', 'required'],
-  //
-  'Enum True2': { enum: [true] },
-  'Enum True Custom Message': { enum: [true], message: 'message' },
-  //
-  'Type Restriction': { type: Date },
-  //
-  'Never Enum': { enum: [] },
-  'Never Enum2': { enum: true },
-  'Never Empty': {},
-  'Never Knot': [{ string: true }, { boolean: true }, { required: true }],
-  'Never Knot2': ['string', 'boolean', 'required'],
-  'Complex Knot': { string: true, boolean: true, enum: [1, 2, 3] },
-};
-
-/**
- * NN: Not Nullish
- */
-const rr: Record<string, Rules> = {
-  Any1: [],
-  Any2: {},
-  Any3: { message: 'foo' },
-  AnyNN1: 'required',
-  AnyNN2: ['required'],
-  AnyNN3: { required: true },
-  AnyNN4: [{ required: true }, 'required', 'required'],
-  AnyNNC1: { required: 'foo' },
-  AnyNNC2: { required: true, message: 'foo' },
-  AnyNNC3: [{ required: true, message: 'foo' }, 'required', 'required'],
-  //
-  String1: 'string',
-  String2: ['string'],
-  String3: { string: true },
-  String4: [{ string: true }, 'string'],
-  StringNN1: ['string', 'required'],
-  StringNN2: [{ string: true, required: true }],
-  StringNN3: [{ string: true, required: true }, 'string', 'required'],
-  StringNN4: [{ required: true }, 'string'],
-  StringNN5: ['required', { string: true }],
-  StringC: { string: true, message: 'foo' },
-  StringNNC: { string: true, required: 'foo' },
-  StringCNNC: { string: 'foo', required: 'bar' },
-  StringExact: { string: 3 },
-  StringExact2: { string: [3, 3] },
-  StringMin: { string: [3] },
-  StringMax: { string: [, 10] },
-  StringRange1: { string: [3, 10] },
-  StringRange2: { string: [-Infinity, Infinity] },
-  StringRangeC1: { string: [3, 10], message: 'foo' },
-  StringRangeC2: [
-    { string: true, message: 'foo' },
-    { string: [3, 10], message: 'bar' },
-  ],
-  StringRangeC3: [
-    { string: 'foo', required: 'duang' },
-    { string: [3, 10], message: 'bar' },
-  ],
-  //
-  'Number Nullish Literal': 'number',
-  'Number Type Required': ['number', 'required'],
-  'Number Min': { number: [3] },
-  'Number Max': { number: [, 10] },
-  'Number Range': { number: [3, 10] },
-  //
-  'Formats Email': { email: true },
-  'Formats Email Literal': 'email',
-  'Formats Email Literal Required': ['email', 'required'],
-  //
-  'Enum True2': { enum: [true] },
-  'Enum True Custom Message': { enum: [true], message: 'message' },
-  //
-  'Type Restriction': { type: Date },
-  //
-  'Never Enum': { enum: [] },
-  'Never Enum2': { enum: true },
-  'Never Empty': {},
-  'Never Knot': [{ string: true }, { boolean: true }, { required: true }],
-  'Never Knot2': ['string', 'boolean', 'required'],
-  'Complex Knot': { string: true, boolean: true, enum: [1, 2, 3] },
+  // String___1: d('string'),
+  // String___2: d(['string']),
+  // String___3: d({ type: 'string' }),
+  // StringNN_1: d('string!'),
+  // StringNN_2: d(['string!']),
+  // StringNN_3: d({ type: 'string', required: true }),
+  // String__C1: d({ type: 'string', message: 'foo' }),
+  // StringNNC1: d({ type: 'string', required: true, message: 'foo' }),
+  // StringR__A: d(['string', 3]),
+  // StringR__B: d({ type: 'string', config: 3 }),
+  // StringR__C: d({ type: 'string', config: [3, 3] }),
+  // StringR__D: d({ type: 'string', config: [3] }),
+  // StringR__E: d({ type: 'string', config: [, 10] }),
+  // StringR__F: d({ type: 'string', config: [3, 10] }),
+  // StringRN_1: d(['string!', 3]),
+  // StringNN5: ['required', { string: true }],
+  // StringC: { type: 'string', message: 'foo' },
+  // StringNNC: { type: 'string', required: 'foo' },
+  // StringCNNC: { string: 'foo', required: 'bar' },
+  // StringExact: { string: 3 },
+  // StringExact2: { string: [3, 3] },
+  // StringMin: { string: [3] },
+  // StringMax: { string: [, 10] },
+  // StringRange1: { string: [3, 10] },
+  // StringRange2: { string: [-Infinity, Infinity] },
+  // StringRangeC1: { string: [3, 10], message: 'foo' },
+  // StringRangeC2: [
+  //   { string: true, message: 'foo' },
+  //   { string: [3, 10], message: 'bar' },
+  // ],
+  // StringRangeC3: [
+  //   { string: 'foo', required: 'duang' },
+  //   { string: [3, 10], message: 'bar' },
+  // ],
+  // //
+  // 'Number Nullish Literal': 'number',
+  // 'Number Type Required': ['number', 'required'],
+  // 'Number Min': { number: [3] },
+  // 'Number Max': { number: [, 10] },
+  // 'Number Range': { number: [3, 10] },
+  // //
+  // 'Formats Email': { email: true },
+  // 'Formats Email Literal': 'email',
+  // 'Formats Email Literal Required': ['email', 'required'],
+  // //
+  // 'Enum True2': { enum: [true] },
+  // 'Enum True Custom Message': { enum: [true], message: 'message' },
+  // //
+  // 'Type Restriction': { type: Date },
+  // //
+  // 'Never Enum': { enum: [] },
+  // 'Never Enum2': { enum: true },
+  // 'Never Empty': {},
+  // 'Never Knot': [{ string: true }, { boolean: true }, { required: true }],
+  // 'Never Knot2': ['string', 'boolean', 'required'],
+  // 'Complex Knot': { string: true, boolean: true, enum: [1, 2, 3] },
 };
 
 describe('vadliator', () => {
   const $ =
-    (...rulesss: Rules[]) =>
+    (...rulesss: RuleS<keyof IRuleType>[]) =>
     (...turples: [any, string][]) => {
       rulesss.forEach(rules =>
         turples.forEach(([v, exp]) => {
           expect(rules).toBeTruthy();
-          const { ok, message } = validate(v, rules, 'Dinner');
-          expect(ok ? 'ok' : message).toBe(exp);
+          const message = validate(v, rules, 'Dinner') ?? 'ok';
+          expect(message).toBe(exp);
         }),
       );
     };
 
   it('validates any', () => {
-    $(rr['Any1'], rr['Any2'], rr['Any3'])(
+    $(rr.Any___1, rr.Any___2, rr.Any___3)(
       [0, 'ok'],
       [-1.1, 'ok'],
       [NaN, 'ok'],
@@ -188,8 +112,8 @@ describe('vadliator', () => {
     );
   });
 
-  it('validates not nullish', () => {
-    $(rr['AnyNN1'], rr['AnyNN2'], rr['AnyNN3'], rr['AnyNN4'])(
+  it('validates any not nullish', () => {
+    $(rr.AnyNN_1, rr.AnyNN_2, rr.AnyNN_3, rr.AnyNN_4)(
       [0, 'ok'],
       [false, 'ok'],
       ['', 'ok'],
@@ -198,7 +122,7 @@ describe('vadliator', () => {
       [[], 'ok'],
       [[[]], 'ok'],
     );
-    $(rr['AnyNNC1'], rr['AnyNNC2'], rr['AnyNNC3'])(
+    $(rr.AnyNNC1)(
       [0, 'ok'],
       [false, 'ok'],
       ['', 'ok'],
@@ -210,7 +134,7 @@ describe('vadliator', () => {
   });
 
   it('validates string', () => {
-    $(rr['String1'], rr['String2'], rr['String3'], rr['String4'])(
+    $('string')(
       [0, 'Dinner 必须为字符类型'],
       [[], 'Dinner 必须为字符类型'],
       ['', 'ok'],
@@ -221,44 +145,57 @@ describe('vadliator', () => {
       [undefined, 'ok'],
     );
 
-    $(
-      rr['StringNN1'],
-      // rr['StringNN2'],
-      // rr['StringNN3'],
-      // rr['StringNN4'],
-      // rr['StringNN5'],
-    )(
+    $('string!')(
       [0, 'Dinner 必须为字符类型'],
       [[], 'Dinner 必须为字符类型'],
       ['', 'Dinner 是必填项'],
-      // ['  ', 'ok'],
-      // ['\n', 'ok'],
-      // ['  zoo', 'ok'],
-      // [null, 'ok'],
-      // [undefined, 'ok'],
+      ['  ', 'ok'],
+      ['\n', 'ok'],
+      ['  zoo', 'ok'],
+      [null, 'ok'],
+      [undefined, 'ok'],
     );
-    // $(
-    //   rr['StringNN1'],
-    //   rr['StringNN2'],
-    //   rr['StringNN3'],
-    //   rr['StringNN4'],
-    //   rr['StringNN5'],
-    // )(
-    //   [0, 'Dinner 必须为字符类型'],
-    //   [[], 'Dinner 必须为字符类型'],
-    //   ['', 'Dinner 是必填项'],
-    //   ['  zoo', 'ok'],
-    //   [null, 'Dinner 是必填项'],
-    //   [undefined, 'Dinner 是必填项'],
-    // );
+  });
 
-    // $(rr['StringC'])(
-    //   [0, 'foo'],
-    //   [[], 'foo'],
-    //   ['', 'Dinner 是必填项'],
-    //   ['  zoo', 'ok'],
-    //   [null, 'Dinner 是必填项'],
-    //   [undefined, 'Dinner 是必填项'],
+  it('validates number', () => {
+    $('number')(
+      [0, 'ok'],
+      [-1.1, 'ok'],
+      [NaN, 'Dinner 必须为数值类型'],
+      [Infinity, 'Dinner 必须为数值类型'],
+      [() => {}, 'Dinner 必须为数值类型'],
+      [Symbol.iterator, 'Dinner 必须为数值类型'],
+      [Date, 'Dinner 必须为数值类型'],
+      [false, 'Dinner 必须为数值类型'],
+      [true, 'Dinner 必须为数值类型'],
+      [String, 'Dinner 必须为数值类型'],
+      ['111', 'Dinner 必须为数值类型'],
+      ['', 'Dinner 必须为数值类型'],
+      [null, 'ok'],
+      [undefined, 'ok'],
+      // [JSON, 'Dinner 必须为数值类型'],
+      // [[undefined], 'Dinner 必须为数值类型'],
+      // [{}, 'Dinner 必须为数值类型'],
+    );
+
+    // $('number!')(
+    //   [0, 'ok'],
+    //   [-1.1, 'ok'],
+    //   [NaN, 'Dinner 必须为数值类型'],
+    //   [Infinity, 'Dinner 必须为数值类型'],
+    //   [() => {}, 'Dinner 必须为数值类型'],
+    //   [Symbol.iterator, 'Dinner 必须为数值类型'],
+    //   [Date, 'Dinner 必须为数值类型'],
+    //   [false, 'Dinner 必须为数值类型'],
+    //   [true, 'Dinner 必须为数值类型'],
+    //   [String, 'Dinner 必须为数值类型'],
+    //   ['111', 'Dinner 必须为数值类型'],
+    //   ['', 'Dinner 必须为数值类型'],
+    //   [null, 'Dinner 必须为数值类型'],
+    //   [undefined, 'Dinner 必须为数值类型'],
+    //   [JSON, 'Dinner 必须为数值类型'],
+    //   [[undefined], 'Dinner 必须为数值类型'],
+    //   [{}, 'Dinner 必须为数值类型'],
     // );
   });
 });
--- packages/strawberry-farm/functions/validator.ts
+++ packages/strawberry-farm/functions/validator.ts
@@ -1,16 +1,78 @@
-const messages = {
-  _field: '字段',
+export interface IRuleType {
+  any: undefined;
+  string: number | (number | null | undefined)[] | RegExp;
+  number: number | (number | null | undefined)[];
+  enum: any[];
+  boolean: undefined;
+  checked: undefined;
+  email: undefined;
+  array: number | (number | null | undefined)[];
+  // object: undefined;
+  type: any;
+}
 
+export interface IRule<T extends keyof IRuleType=  keyof IRuleType> {
+  type?: T;
+  config?: IRuleType[T];
+  required?: boolean;
+  validator?(value: unknown): string | undefined | Promise<string | undefined>;
+  message?: string;
+}
+
+export type RuleS<T extends keyof IRuleType=  keyof IRuleType> =
+  | IRule<T>
+  | T
+  | `${T}!`
+  | [T | `${T}!`]
+  | [T | `${T}!`, IRuleType[T]];
+
+const unpack = <T extends keyof IRuleType>(mini: RuleS<T>): IRule<T> => {
+  if (typeof mini === 'object' && !Array.isArray(mini)) return mini;
+
+  const [a, b] = [
+    Array.isArray(mini) ? mini[0] : mini,
+    Array.isArray(mini) ? mini[1] : undefined,
+  ];
+
+  let rule: IRule = {
+    type: (a.endsWith('!') ? a.substring(0, -1) : a) as T,
+  };
+
+  if (a.endsWith('!')) {
+    rule.required = true;
+  }
+
+  if (b) {
+    rule.config = b;
+  }
+
+  return rule;
+};
+
+const defaults = {
+  fieldname: '字段',
+};
+
+const messages: Record<keyof IRuleType | 'default' | 'required', string> & {
+  ranges: Partial<
+    Record<
+      keyof IRuleType,
+      { len: string; min: string; max: string; range: string }
+    >
+  >;
+} = {
   default: '%s 不匹配格式',
   required: '%s 是必填项',
-  checked: '勾选 %s 以继续',
+  checked: '请先检阅 %s',
   enum: '%s 不在选项范围内',
 
+  any: '%s 必须为任意类型',
   string: '%s 必须为字符类型',
   array: '%s 必须为列表类型',
   number: '%s 必须为数值类型',
   boolean: '%s 必须为布尔类型',
   email: '%s 不是有效的邮箱地址',
+  type: '%s 不匹配格式',
 
   ranges: {
     string: {
@@ -44,273 +106,157 @@ const testRange = (
   messagegroup: 'number' | 'string' | 'array',
 ) => {
   if (typeof range === 'number' && value !== range) {
-    return messages.ranges[messagegroup].len.replace(aRE, '' + range);
+    return messages.ranges[messagegroup]!.len.replace(aRE, '' + range);
   } else if (Array.isArray(range)) {
     const [min, max] = range;
     if (typeof min === 'number' && value < min)
       return min === max
-        ? messages.ranges[messagegroup].range
-            .replace(aRE, '' + min)
-            .replace(bRE, '' + max)
-        : messages.ranges[messagegroup].min.replace(aRE, '' + min);
+        ? messages.ranges[messagegroup]!.range.replace(aRE, '' + min).replace(
+            bRE,
+            '' + max,
+          )
+        : messages.ranges[messagegroup]!.min.replace(aRE, '' + min);
     else if (typeof max === 'number' && value > max)
       return min === max
-        ? messages.ranges[messagegroup].range
-            .replace(aRE, '' + min)
-            .replace(bRE, '' + max)
-        : messages.ranges[messagegroup].max.replace(aRE, '' + max);
+        ? messages.ranges[messagegroup]!.range.replace(aRE, '' + min).replace(
+            bRE,
+            '' + max,
+          )
+        : messages.ranges[messagegroup]!.max.replace(aRE, '' + max);
   }
 };
 
-const emptyRulers = new Map<string, any>(
-  Object.entries({
-    string: (s: string) => s === '',
-  }),
-);
+const validators2 = new Map<
+  keyof IRuleType,
+  {
+    type: (value: unknown, config: unknown) => string | void;
+    required?(value: unknown, config: unknown): boolean;
+    extends?: keyof IRuleType;
+  }
+>();
 
-const validators = new Map<string, any>(
-  Object.entries({
-    string: (value: unknown, config: unknown) => {
-      if (typeof value !== 'string') return messages.string;
+validators2.set('any', {
+  type: () => {},
+});
 
-      if (typeof config === 'number' || Array.isArray(config)) {
-        return testRange(value.length, config, 'string');
-      }
-      if (config instanceof RegExp) {
-        if (!config.test(value)) {
-          return messages.default;
-        }
-      }
-    },
-    boolean: (value: unknown, config: unknown) => {
-      if (typeof value !== 'boolean') return messages.boolean;
-    },
-    number: (value: unknown, config: unknown) => {
-      if (
-        typeof value !== 'number' ||
-        !Number.isFinite(value) ||
-        Number.isNaN(value)
-      )
-        return messages.number;
+validators2.set('string', {
+  type: (value: unknown, config: unknown) => {
+    if (typeof value !== 'string') return messages.string;
 
-      if (typeof config === 'number' || Array.isArray(config)) {
-        return testRange(value, config, 'number');
-      }
-    },
-    array: (value: unknown, config: unknown) => {
-      if (!Array.isArray(value)) {
-        return messages.array;
-      }
-
-      if (typeof config === 'number' || Array.isArray(config)) {
-        return testRange(value.length, config, 'array');
-      }
-    },
-    enum: (value: unknown, config: unknown) => {
-      const _config = Array.isArray(config) ? config : [config];
-      if (!_config.length || _config.every(item => !Object.is(value, item))) {
-        return messages.enum;
-      }
-    },
-    type: (value: unknown, config: unknown) => {
-      if (config && value && !(value instanceof (config as any))) {
+    if (typeof config === 'number' || Array.isArray(config)) {
+      return testRange(value.length, config, 'string');
+    }
+    if (config instanceof RegExp) {
+      if (!config.test(value)) {
         return messages.default;
       }
-    },
-    checked: (value: unknown) => {
-      if (value !== true) return messages.checked;
-    },
-    /**
-     * Used: https://html.spec.whatwg.org/multipage/input.html#email-state-(type=email)
-     * Candidate: https://github.com/ajv-validator/ajv-formats/blob/4dd65447575b35d0187c6b125383366969e6267e/src/formats.ts#L64
-     * Candidate: https://emailregex.com/
-     */
-    email:
-      /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-  }),
-);
+    }
+  },
+  required: (s: string) => s === '',
+});
 
-interface IRuleType {
-  string: number | number[] | RegExp;
-  number: number | number[];
-  enum: any[];
-  boolean: undefined;
-  checked: undefined;
-  email: undefined;
-  array: number | number[];
-  object: undefined;
-}
+validators2.set('number', {
+  type: (value: unknown, config: unknown) => {
+    if (
+      typeof value !== 'number' ||
+      !Number.isFinite(value) ||
+      Number.isNaN(value)
+    )
+      return messages.number;
 
-interface IRule2 {
-  // string?: boolean | string | number | (number | null | undefined)[] | RegExp;
-  // number?: boolean | string | number | (number | null | undefined)[];
-  // boolean?: boolean | string;
-  // checked?: boolean | string;
-  // email?: boolean | string;
-  // array?: boolean | string;
-  // object?: boolean | string | {};
-  type?: keyof IRuleType | any;
-  // enum?:
-  //   | string
-  //   | number
-  //   | boolean
-  //   | null
-  //   | undefined
-  //   | (string | number | boolean | null | undefined)[];
+    if (typeof config === 'number' || Array.isArray(config)) {
+      return testRange(value, config, 'number');
+    }
+  },
+});
 
-  // Meta fields
-  config?: any;
-  required?: boolean;
-  validator?(value: unknown): string | undefined | Promise<string | undefined>;
-  message?: string;
-}
+validators2.set('array', {
+  type: (value: unknown, config: unknown) => {
+    if (!Array.isArray(value)) {
+      return messages.array;
+    }
 
-type RuleMini =
-  | IRule2
-  | keyof IRuleType
-  | `${keyof IRuleType} required`
-  | 'required';
+    if (typeof config === 'number' || Array.isArray(config)) {
+      return testRange(value.length, config, 'array');
+    }
+  },
+});
 
-type EndWith<T> = [...T[], string];
+validators2.set('enum', {
+  type: (value: unknown, config: unknown) => {
+    const _config = Array.isArray(config) ? config : [config];
+    if (_config.length && !_config.some(item => Object.is(value, item))) {
+      return messages.enum;
+    }
+  },
+});
 
-const a: RuleMini | RuleMini[] | EndWith<RuleMini> = [
-  'string',
-  'required',
-  'required',
-  'string required',
-  'qwer',
-  'checked'
-];
+validators2.set('boolean', {
+  type: (value: unknown) => {
+    if (typeof value !== 'boolean') return messages.boolean;
+  },
+});
 
-// {
-//   string?: boolean | string | number | (number | null | undefined)[] | RegExp;
-//   number?: boolean | string | number | (number | null | undefined)[];
-//   boolean?: boolean | string;
-//   checked?: boolean | string;
-//   email?: boolean | string;
-//   array?: boolean | string;
-//   object?: boolean | string | {};
-//   type?: any;
-//   enum?:
-//     | string
-//     | number
-//     | boolean
-//     | null
-//     | undefined
-//     | (string | number | boolean | null | undefined)[];
+validators2.set('checked', {
+  type: (value: unknown) => {
+    if (value !== true) return messages.checked;
+  },
+});
 
-//   // Meta fields
-//   required?: string | boolean;
-//   validator?(value: unknown): string | undefined | Promise<string | undefined>;
-//   message?: string;
-// }
+validators2.set('type', {
+  type: (value: unknown, config: unknown) => {
+    if (config && value && !(value instanceof (config as any))) {
+      return messages.default;
+    }
+  },
+});
 
-interface IRule {
-  string?: boolean | string | number | (number | null | undefined)[] | RegExp;
-  number?: boolean | string | number | (number | null | undefined)[];
-  boolean?: boolean | string;
-  checked?: boolean | string;
-  email?: boolean | string;
-  array?: boolean | string;
-  object?: boolean | string | {};
-  type?: any;
-  enum?:
-    | string
-    | number
-    | boolean
-    | null
-    | undefined
-    | (string | number | boolean | null | undefined)[];
+/**
+ * Used: https://html.spec.whatwg.org/multipage/input.html#email-state-(type=email)
+ * Candidate: https://github.com/ajv-validator/ajv-formats/blob/4dd65447575b35d0187c6b125383366969e6267e/src/formats.ts#L64
+ * Candidate: https://emailregex.com/
+ */
+const EMAILRE =
+  /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-  // Meta fields
-  required?: string | boolean;
-  validator?(value: unknown): string | undefined | Promise<string | undefined>;
-  message?: string;
-}
-
-export type Rule = IRule | keyof IRule;
-export type Rules = Rule | Rule[];
-
-interface Validate {
-  (
-    value: unknown,
-    rules: Rules,
-    name?: string,
-  ): { ok: boolean; message: string };
-}
+validators2.set('email', {
+  extends: 'string',
+  type(value) {
+    if (!EMAILRE.test(value as string)) return messages.email;
+  },
+});
 
 const testNullish = (value: unknown) => value === null || value === undefined;
 
-export const validate: Validate = (value, rules, name = messages._field) => {
-  if (!Array.isArray(rules)) rules = [rules];
+export const validate = <T extends keyof IRuleType>(
+  value: unknown,
+  _rule: RuleS<T>,
+  name = defaults.fieldname,
+): void | string | Promise<void | string> => {
+  const rule = unpack(_rule);
 
-  let message: undefined | string;
+  const v = rule.type && validators2.get(rule.type);
 
-  for (let rule of rules) {
-    if (typeof rule === 'string') rule = { [rule]: true };
-
-    const required = typeof rule.required === 'string' || rule.required;
-    const messageOfRequired =
-      typeof rule.required === 'string'
-        ? rule.required
-        : rule.required
-          ? messages.required.replace(sRE, name)
-          : undefined;
-
-    if (!testNullish(value)) {
-      for (const key in rule) {
-        if (key === 'required') continue;
-        if (key === 'validator') continue;
-        if (key === 'message') continue;
-
-        let validator = validators.get(key);
-        if (validator) {
-          if (validator instanceof RegExp) {
-            validator = value => {
-              if (typeof value === 'string' && !validator.test(value)) {
-                return messages[key] ?? messages.default;
-              }
-            };
-          }
-
-          const config = rule[key as keyof IRule];
-          message = validator(value as any, config);
-          if (typeof message === 'string') {
-            message = message.replace(sRE, name);
-            if (typeof config === 'string') message = config;
-            break;
-          }
-        }
-
-        const emptyRuler = emptyRulers.get(key);
-        if (required && emptyRuler && emptyRuler(value)) {
-          message = messageOfRequired;
-          break;
-        }
-
-        if (key === 'validator') {
-          const validator = rule[key as 'validator']!;
-
-          if (
-            typeof (
-              (message = validator(value) as string | undefined) === 'string'
-            )
-          ) {
-            break;
-          }
-        }
-      }
-    } else {
-      if (required) {
-        message = messageOfRequired;
-      }
-    }
-
-    if (typeof message === 'string') {
-      message = rule.message ?? message;
-      break;
-    }
+  if (v?.extends) {
+    const message = validate(value, v.extends, name);
+    if (message) return message;
   }
 
-  return { ok: typeof message !== 'string', message: message || '' };
+  if (
+    rule.required &&
+    (testNullish(value) || v?.required?.(value, rule.config))
+  ) {
+    return rule.message ?? messages.required.replace(sRE, name);
+  }
+
+  let storedValidatorErrorMessage = v?.type?.(value, rule.config);
+  if (typeof storedValidatorErrorMessage === 'string') {
+    return rule.message ?? storedValidatorErrorMessage.replace(sRE, name);
+  }
+
+  return rule.validator?.(value);
 };
+
+export const defineRule = <T extends keyof IRuleType>(_: RuleS<T>) => _;
+
+const r = validate('1', 'any');
