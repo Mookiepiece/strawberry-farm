656357c8fb418a758e460c7dfe7eda309e33bd04
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2024-02-07 18:28:15 +0800

    z

--- packages/docs/.vitepress/theme/Layout.vue
+++ packages/docs/.vitepress/theme/Layout.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import { useRoute } from 'vitepress'
-import { computed, provide, useSlots, watch } from 'vue'
-import VPBackdrop from 'vitepress/dist/client/theme-default/components/VPBackdrop.vue'
+import {  watch } from 'vue'
 import VPContent from 'vitepress/dist/client/theme-default/components/VPContent.vue'
-import VPFooter from 'vitepress/dist/client/theme-default/components/VPFooter.vue'
-import VPLocalNav from 'vitepress/dist/client/theme-default/components/VPLocalNav.vue'
-import VPNav from 'vitepress/dist/client/theme-default/components/VPNav.vue'
 import VPSidebar from 'vitepress/dist/client/theme-default/components/VPSidebar.vue'
 import { useData } from 'vitepress/dist/client/theme-default/composables/data'
 import { useCloseSidebarOnEscape, useSidebar } from 'vitepress/dist/client/theme-default/composables/sidebar'
@@ -23,25 +19,14 @@ useCloseSidebarOnEscape(isSidebarOpen, closeSidebar)
 
 const { frontmatter } = useData()
 
-const slots = useSlots()
-const heroImageSlotExists = computed(() => !!slots['home-hero-image'])
-
-provide('hero-image-slot-exists', heroImageSlotExists)
 </script>
 
 <template>
-  <div v-if="frontmatter.layout !== false" class="Layout" :class="frontmatter.pageClass">
-    <VPBackdrop class="backdrop" :show="isSidebarOpen" @click="closeSidebar" />
-    <VPNav>
-    </VPNav>
-    <VPLocalNav :open="isSidebarOpen" @open-menu="openSidebar" />
-
+  <div class="Layout">
     <VPSidebar :open="isSidebarOpen">
     </VPSidebar>
     <VPContent />
-    <VPFooter />
   </div>
-  <Content v-else />
 </template>
 
 <style scoped>
