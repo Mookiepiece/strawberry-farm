33faefb7c5c88194b8140a923fcd0209deb8a22a
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2024-02-21 08:56:09 +0800

    z

--- packages/docs/html/Form.vue
+++ packages/docs/html/Form.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
 import VInput from '@mookiepiece/strawberry-farm/vue/VInput.vue';
-
-
+import VInputGroup from '@mookiepiece/strawberry-farm/vue/VInputGroup.vue';
 </script>
 
 <template>
-  <div class="[A]" style="gap: 10px;">
+  <div class="[A]" style="gap: 10px">
     <VInput placeholder="Please Input" />
     <VInput placeholder="Please Input" class="[m]" />
     <VInput placeholder="Please Input" class="[l]" />
+
+    <div class="sf-input-group [B]">
+      <ins>https://</ins>
+      <VInput placeholder="Please Input" />
+      <ins>.com</ins>
+    </div>
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
