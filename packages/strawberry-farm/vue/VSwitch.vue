f20856ee92bd77e6638f578ada4b75998a766160
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2024-04-30 08:49:27 +0800

    VSwitch: proposal

---
+++ packages/strawberry-farm/vue/VSwitch.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const model = defineModel();
+
+defineProps<{
+  disabled?: boolean;
+}>();
+
+const el = ref<HTMLDivElement>();
+
+defineExpose({ el });
+</script>
+
+<template>
+  <div
+    :tabindex="disabled ? '-1' : '0'"
+    class="VSwitch"
+    :aria-disabled="disabled"
+    :aria-pressed="model ? 'true' : 'false'"
+  >
+    <slot />
+    <div class="VSwitchIndicator"></div>
+  </div>
+</template>
