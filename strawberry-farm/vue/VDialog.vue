8a28e3f307b5f6beadbe6239483903adba953818
彼術向 <48076971+Mookiepiece@users.noreply.github.com>
2024-09-29 12:51:07 +0800

    theme: width

--- strawberry-farm/vue/VDialog.vue
+++ strawberry-farm/vue/VDialog.vue
@@ -1,15 +1,29 @@
 <script setup lang="ts">
+import { computed } from 'vue';
 import VModal from './VModal.vue';
 
 const model = defineModel();
-const props = defineProps<{
-  title?: string;
-  strong?: boolean;
-}>();
+const props = withDefaults(
+  defineProps<{
+    width?: number;
+    title?: string;
+    strong?: boolean;
+  }>(),
+  { width: 800 },
+);
+
+const width = computed(() =>
+  typeof props.width === 'number' ? props.width + 'px' : props.width,
+);
 </script>
 
 <template>
-  <VModal class="VDialog" v-model="model" :strong="strong">
+  <VModal
+    class="VDialog"
+    :style="{ '--v-width': width }"
+    v-model="model"
+    :strong
+  >
     <div class="VDialogHeader">
       {{ title }}
       <kbd class="ml-auto">Esc</kbd>
